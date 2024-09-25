28c1b5a578bf305d8072bbe811fbfe1a4af1370f
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2024-09-25 22:41:38 +0900

    feat: 플레이어 추격 enemy 구현

---
+++ Part1/Assets/Script/Enemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject target;
+    NavMeshAgent agent;
+    Animator animator;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.transform.position;
+        animator.SetFloat("Speed", agent.velocity.magnitude);
+    }
+}
